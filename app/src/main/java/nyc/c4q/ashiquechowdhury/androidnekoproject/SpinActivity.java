3ad4fa58efce32a8429bd12ba6bf218416eedba0
achowdhury3762 <achowdhury3762@gmail.com>
2016-12-16 16:51:21 -0500

    Added alert dialog to activity

--- app/src/main/java/nyc/c4q/ashiquechowdhury/androidnekoproject/SpinActivity.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/androidnekoproject/SpinActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
  * Created by ashiquechowdhury on 12/12/16.
  */
 public class SpinActivity extends AppCompatActivity{
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
