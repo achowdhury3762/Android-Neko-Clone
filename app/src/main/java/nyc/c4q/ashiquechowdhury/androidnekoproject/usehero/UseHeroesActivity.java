ef32d338fd6395833a9222f6ab274d978bb347c8
achowdhury3762 <achowdhury3762@gmail.com>
2016-12-19 23:17:42 -0500

    Changed superhero names

--- app/src/main/java/nyc/c4q/ashiquechowdhury/androidnekoproject/usehero/UseHeroesActivity.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/androidnekoproject/usehero/UseHeroesActivity.java
@@ -1,6 +1,7 @@
 package nyc.c4q.ashiquechowdhury.androidnekoproject.usehero;
 
 
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 import com.nightonke.boommenu.BoomButtons.TextInsideCircleButton;
 import com.nightonke.boommenu.BoomMenuButton;
 import com.nightonke.boommenu.OnBoomListenerAdapter;
+import com.nightonke.boommenu.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,18 +23,23 @@ import nyc.c4q.ashiquechowdhury.androidnekoproject.util.RandomNumberChooser;
  */
 
 public class UseHeroesActivity extends AppCompatActivity {
-    @BindView(R.id.total_hero_tView) TextView totalHeroesTView;
-    @BindView(R.id.bmb) BoomMenuButton nineMenuButton;
-    @BindView(R.id.bmb2) BoomMenuButton oneMenuButton;
-    MySharedPreferences sharedPrefs;
+    @BindView(R.id.total_hero_tView)
+    TextView totalHeroesTView;
+    @BindView(R.id.bmb)
+    BoomMenuButton nineMenuButton;
+    @BindView(R.id.bmb2)
+    BoomMenuButton oneMenuButton;
+    private MySharedPreferences sharedPrefs;
+    private Hero[] myHeroes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.boommenu_spinner);
         ButterKnife.bind(this);
+        sharedPrefs = MySharedPreferences.getInstance(getApplicationContext());
 
-        Hero[] myHeroes = new Hero[16];
+        myHeroes = new Hero[16];
         myHeroes[0] = new Hero("AQUAMAN", R.drawable.aquaman);
         myHeroes[1] = new Hero("BATMAN", R.drawable.aquaman);
         myHeroes[2] = new Hero("BLACKWIDOW", R.drawable.blackwidow);
@@ -42,44 +49,31 @@ public class UseHeroesActivity extends AppCompatActivity {
         myHeroes[6] = new Hero("HAWKEYE", R.drawable.hawkeye);
         myHeroes[7] = new Hero("GREENLANTERN", R.drawable.greenlantern);
         myHeroes[8] = new Hero("FLASH", R.drawable.flash);
-        myHeroes[9] = new Hero("NICKFURY", R.drawable.nickfury);
-        myHeroes[10] = new Hero("WONDERWOMAN", R.drawable.wonderwoman);
-        myHeroes[11] = new Hero("THOR", R.drawable.thor);
-        myHeroes[12] = new Hero("SUPERMAN", R.drawable.superman);
-        myHeroes[13] = new Hero("SPIDERMAN", R.drawable.spiderman);
+        myHeroes[9] = new Hero("NICKFURY", R.drawable.nickfury_icon);
+        myHeroes[10] = new Hero("WONDERWOMAN", R.drawable.wonderwoman_icon);
+        myHeroes[11] = new Hero("THOR", R.drawable.thor_icon);
+        myHeroes[12] = new Hero("SUPERMAN", R.drawable.superman_icon);
+        myHeroes[13] = new Hero("SPIDERMAN", R.drawable.spiderman_icon);
         myHeroes[14] = new Hero("GREENARROW", R.drawable.greenarrow);
         myHeroes[15] = new Hero("HULK", R.drawable.hulk);
 
-        sharedPrefs = MySharedPreferences.getInstance(getApplicationContext());
+        setTotalHeroText(totalHeroesTView, sharedPrefs.getTotalHeroCount());
 
-        setText(totalHeroesTView, sharedPrefs.getTotalHeroCount());
-        for (int i = 0; i < nineMenuButton.getPiecePlaceEnum().pieceNumber(); i++) {
-            int randNum = RandomNumberChooser.chooseRandomNumber(15);
-            Hero randomHero = myHeroes[randNum];
-
-            TextInsideCircleButton.Builder builder = new TextInsideCircleButton.Builder()
-                    .normalImageRes(randomHero.getDrawable())
-                    .buttonRadius(100)
-                    .normalText("Hero Earned!");
-            nineMenuButton.addBuilder(builder);
-        }
-
-        for (int i = 0; i < oneMenuButton.getPiecePlaceEnum().pieceNumber(); i++) {
-            int randNum = RandomNumberChooser.chooseRandomNumber(15);
-            Hero randomHero = myHeroes[randNum];
-            TextInsideCircleButton.Builder builder = new TextInsideCircleButton.Builder()
-                    .normalImageRes(randomHero.getDrawable())
-                    .normalText("Hero Earned!");
-            oneMenuButton.addBuilder(builder);
-        }
-
+        setUpNineHeroClickedButton();
+        setUpOneHeroClickedButton();
 
         nineMenuButton.setOnBoomListener(new OnBoomListenerAdapter() {
             @Override
             public void onBoomWillShow() {
                 super.onBoomWillShow();
-                sharedPrefs.decreaseTotalHeroCount(1);
-                setText(totalHeroesTView, sharedPrefs.getTotalHeroCount());
+                sharedPrefs.decreaseTotalHeroCount(9);
+                setTotalHeroText(totalHeroesTView, sharedPrefs.getTotalHeroCount());
+            }
+
+            @Override
+            public void onBoomDidHide() {
+                super.onBoomDidHide();
+                recreate();
             }
         });
 
@@ -87,13 +81,47 @@ public class UseHeroesActivity extends AppCompatActivity {
             @Override
             public void onBoomWillShow() {
                 super.onBoomWillShow();
-                sharedPrefs.decreaseTotalHeroCount(9);
-                setText(totalHeroesTView, sharedPrefs.getTotalHeroCount());
+                sharedPrefs.decreaseTotalHeroCount(1);
+                setTotalHeroText(totalHeroesTView, sharedPrefs.getTotalHeroCount());
+            }
+
+            @Override
+            public void onBoomDidHide() {
+                super.onBoomDidHide();
+                recreate();
             }
         });
+
     }
 
-    private void setText(TextView myTextView, int spinCount) {
+    private void setUpOneHeroClickedButton() {
+        nineMenuButton.setCacheOptimization(false);
+        oneMenuButton.setCacheOptimization(false);
+        for (int i = 0; i < oneMenuButton.getPiecePlaceEnum().pieceNumber(); i++) {
+            int randNum = RandomNumberChooser.chooseRandomNumber(15);
+            Hero randomHero = myHeroes[randNum];
+            TextInsideCircleButton.Builder builder = new TextInsideCircleButton.Builder()
+                    .normalImageRes(randomHero.getDrawable())
+                    .buttonRadius(Util.dp2px(100))
+                    .imageRect(new Rect(Util.dp2px(0), Util.dp2px(0), Util.dp2px(200), Util.dp2px(200)))
+                    .imagePadding(new Rect(Util.dp2px(15), Util.dp2px(15), Util.dp2px(15), Util.dp2px(15)));
+            oneMenuButton.addBuilder(builder);
+        }
+    }
+
+    private void setUpNineHeroClickedButton() {
+        for (int i = 0; i < nineMenuButton.getPiecePlaceEnum().pieceNumber(); i++) {
+            int randNum = RandomNumberChooser.chooseRandomNumber(15);
+            Hero randomHero = myHeroes[randNum];
+
+            TextInsideCircleButton.Builder builder = new TextInsideCircleButton.Builder()
+                    .normalImageRes(randomHero.getDrawable())
+                    .buttonRadius(Util.dp2px(40));
+            nineMenuButton.addBuilder(builder);
+        }
+    }
+
+    private void setTotalHeroText(TextView myTextView, int spinCount) {
         String totalSpinCount = String.valueOf(
                 spinCount
         );
